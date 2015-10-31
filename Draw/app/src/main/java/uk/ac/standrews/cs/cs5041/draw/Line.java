f643222cd9258a38ad1112ad3b2c124314d5e4fe
James Russell <jwrsan@gmail.com>
2015-10-31 17:08:34 +0000

    added line class

---
+++ Draw/app/src/main/java/uk/ac/standrews/cs/cs5041/draw/Line.java
@@ -0,0 +1,13 @@
+package uk.ac.standrews.cs.cs5041.draw;
+
+public class Line {
+    public float x1, y1, x2, y2;
+
+    public Line(float x1, float y1, float x2, float y2)
+    {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+}
