d66d12453e4ccbf42a6464e33dfc6ed758f60979
James Russell <jwrsan@gmail.com>
2015-11-01 15:50:59 +0000

    Rectangle class added

---
+++ Draw/app/src/main/java/uk/ac/standrews/cs/cs5041/draw/Rectangle.java
@@ -0,0 +1,36 @@
+package uk.ac.standrews.cs.cs5041.draw;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class Rectangle implements Shape {
+    public float x1, y1, x2, y2;
+    public Paint paint;
+
+    public Rectangle(float x1, float y1, float x2, float y2, Paint paint)
+    {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+        this.paint = paint;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawRect(x1, y1, x2, y2, paint);
+    }
+
+    @Override
+    public void move(float x, float y) {
+        float centreX, centreY, diffX, diffY;
+        centreX = x1+((x2-x1)/2);
+        centreY = y1+((y2-y1)/2);
+        diffX = x-centreX;
+        diffY = y-centreY;
+        x1 += diffX;
+        x2 += diffX;
+        y1 += diffY;
+        y2 += diffY;
+    }
+}
